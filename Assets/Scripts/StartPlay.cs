abebc87570fb03db5cddd3f2641c518a66c7476c
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-05-04 04:46:32 -0400

    Added start button

---
+++ Assets/Scripts/StartPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPlay : MonoBehaviour
+{
+    public Button btnPlay;
+
+    public void startGame()
+    {
+        btnPlay.gameObject.SetActive(false);
+        Debug.Log("Starting the game");
+    }
+}
