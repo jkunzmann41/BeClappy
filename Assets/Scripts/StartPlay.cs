bffee03de0590302806bdd0d4438af3877a1e7f3
jkunzmann41 <jamiekunzmann@gmail.com>
2019-05-04 10:05:27 -0400

    set metronome and detect taps to active from playstart button

--- Assets/Scripts/StartPlay.cs
+++ Assets/Scripts/StartPlay.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 
 public class StartPlay : MonoBehaviour
 {
+    public GameObject TapScript;
+    public GameObject Metronome;
     public Button btnPlay;
 
     public void startGame()
     {
         btnPlay.gameObject.SetActive(false);
         Debug.Log("Starting the game");
+        Metronome.SetActive(true);
+        TapScript.SetActive(true);
     }
 }
