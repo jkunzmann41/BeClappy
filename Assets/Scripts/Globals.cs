16a7a3d1e6690303bc37640149d9cb46205d1ce2
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-05-04 03:36:49 -0400

    Created map scene and able to change the stars
    '

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
 
 public class Globals: Singleton<Globals>
 {
     public static List<List<Note>> rhythms = new List<List<Note>>() {
        {new List<Note> {Note.EIGHTH, Note.EIGHTH, Note.EIGHTH, Note.EIGHTH, Note.EIGHTH, Note.EIGHTH, Note.EIGHTH, Note.EIGHTH }},
-       {new List<Note> {Note.QUARTER, Note.EIGHTH, Note.EIGHTH, Note.QUARTER, Note.EIGHTH, Note.EIGHTH}}
+       {new List<Note> {Note.QUARTER, Note.EIGHTH, Note.EIGHTH, Note.QUARTER, Note.EIGHTH, Note.EIGHTH}},
+       {new List<Note> {Note.QUARTER, Note.QUARTER, Note.PAUSE, Note.QUARTER}}
     };
     public List<Note> curRhythm;
 
@@ -13,5 +16,26 @@ public class Globals: Singleton<Globals>
 
     public void setRhythm(int i) {
         curRhythm = rhythms[i];
+    }
+    public void setStars(Button btn, int star)
+    {
+        Sprite stars = Resources.Load<Sprite>("Images/" + star + "star");
+
+        //Image image = btn.GetComponentsInChildren;
+        //image.GetComponent<Image>().sprite = stars;
+
+        Image buttonImage = btn.GetComponent<Image>();
+        Image[] images = btn.GetComponentsInChildren<Image>();
+        foreach (Image image in images)
+        {
+            if (image != buttonImage)
+            {
+                image.sprite = stars;
+                break;
+            }
+        }
+        //button.transform.SetParent(shopButtonContrainer.transform, false);
+
+
     }
 }
---
+++ Assets/Scripts/MapRhythmSelection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Rhythm { ALLIGATOR, RUNPONY, ICECREAM };
+public class MapRhythmSelection : MonoBehaviour
+{
+
+     public void OnClicked(Button button)
+     {
+        var selected_index = 0;
+        string btn_name = button.name;
+        if (btn_name == "IceCream")
+        {
+            selected_index = (int)Rhythm.ICECREAM;
+
+        } else if (btn_name == "RunPony")
+        {
+            selected_index = (int)Rhythm.RUNPONY;
+
+        } else if (btn_name == "Alligator")
+        {
+            selected_index = (int)Rhythm.ALLIGATOR;
+
+        }
+
+        Globals.Instance.setRhythm(selected_index);
+        Globals.Instance.setStars(button, 2);
+        Debug.Log("rhythm " + selected_index + " selected " + btn_name);
+
+     }
+}
